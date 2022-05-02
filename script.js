e390c5adf341b2f7fc114bd96f606be6fc7609f2
TGZ7 <togonlopez@gmail.com>
2022-05-02 13:57:48 +0200

    Finished basic version

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ function jump(){
         character.classList.remove("animate");
     },500);
 }
+
+// Constant propieties
+var characterWidth = parseInt(window.getComputedStyle(character).getPropertyValue("width"));
+var characterHeight = parseInt(window.getComputedStyle(character).getPropertyValue("height"));
+var characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+var blockWidth = parseInt(window.getComputedStyle(block).getPropertyValue("width"));
+var blockHeight = parseInt(window.getComputedStyle(block).getPropertyValue("height"));
+var blockTop = parseInt(window.getComputedStyle(block).getPropertyValue("top"));
+
+
+// Check dead
+var checkDead = setInterval(function(){
+    // We obtain the propiety "Top" (in the css) of the variable character
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft > characterLeft && blockLeft < (characterLeft + characterWidth) && characterTop > blockTop ){
+        // We stop the animation
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("You lost");
+    }
+
+},10);
